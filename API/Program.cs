6f69683fe91bc5c1a96ababf9e47a29b9794c875
yadsin1 <yadvendra.jbt@gmail.com>
2025-11-23 16:24:19 +0530

    Working skelton for  Angular client

--- API/Program.cs
+++ API/Program.cs
@@ -8,12 +8,14 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllers();
+builder.Services.AddCors();
 builder.Services.AddDbContext<AppDbContext>(options =>
 {
     options.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
 });
 
 var app = builder.Build();
+app.UseCors(x => x.AllowAnyHeader().AllowAnyMethod().WithOrigins("https://localhost:4200", "http://localhost:4200"));
 
 app.UseHttpsRedirection();
 
